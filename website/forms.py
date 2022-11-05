97d3e7bd207ca70a63226e931b13e6d46beac662
seanlhyde99 <seanlhyde99@gmail.com>
2022-11-05 10:20:49 +1000

    Small Changes to Forms & Heroku Deployment

--- website/forms.py
+++ website/forms.py
@@ -1,23 +1,27 @@
 
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, email_validator
 
 
 #creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
+    email=StringField("Email", validators=[InputRequired('Enter your email')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
+#  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    name=StringField("Name", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
     #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
+    password1=PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+    password2 = PasswordField("Confirm Password")
 
     #submit button
     submit = SubmitField("Register")
+
+class CommentsForm(FlaskForm):
+    textArea=StringField("Comments", validators=[InputRequired])
+    submit = SubmitField("Post comment")
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
@@ -11,16 +11,16 @@ def home():
 
 @views.route('/user_booking')
 def userBooking():
-    return render_template('user_booking.html')
+    return render_template('user_booking.html', user=current_user)
 
 @views.route('/event_creation')
 def eventCreation():
-    return render_template('event_creation.html')
+    return render_template('event_creation.html', user=current_user)
 
 @views.route('/hiphop')
 def hipHop():
-    return render_template('hiphop.html')
+    return render_template('hiphop.html', user=current_user)
 
 @views.route('/event_detail')
 def eventDetails():
-    return render_template('event_detail.html')
+    return render_template('event_detail.html', user=current_user)
